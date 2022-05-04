8cf5090ed484ac1c4ac8fee373dd7754ed86f9cd
Chris Hern <chris.hern@pearson.come>
2022-05-04 14:26:24 -0500

    fix: NewClient test was missing check for env
    Added helper function to get credentials from env variables. In the TestNewClient func, this helper func is called and then a check is run to see if either the user or pass is an empty string. If so, an error is returned

--- devcon_test.go
+++ devcon_test.go
@@ -5,7 +5,18 @@ import (
 	"testing"
 )
 
+func getCredsFromEnv() (string, string) {
+	return os.Getenv("SSH_USER"), os.Getenv("SSH_PASSWORD")
+}
+
 func TestNewClient(t *testing.T) {
-	client := NewClient(os.Getenv("SSH_USER"), os.Getenv("SSH_PASSWORD"), "localhost")
-	t.Logf("IP = %q", client.ip)
+	un, pw := getCredsFromEnv()
+	if un == "" || pw == "" {
+		t.Fatal("env variables not set")
+	}
+	client := NewClient(un, pw, "127.0.0.1")
+	if client.ip == "" {
+		t.Fatalf("\ngot: %q | wanted %q\n", client.ip, os.Getenv("SSH_USER"))
+	}
+	t.Logf("\nuser: %q\nIP: %q", client.clientCfg.User, client.ip)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/montybeatnik/devcon
 
 go 1.17
 
-require (
-	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-)
+require golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f
+
+require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
